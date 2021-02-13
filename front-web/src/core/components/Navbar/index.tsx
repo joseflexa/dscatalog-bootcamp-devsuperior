728f5c9d3cea894381e936e41d64c7289e318ebf
Bruno Toledo <brunotq19@hotmail.com>
2021-02-13 20:07:02 -0300

    Add products details

--- front-web/src/core/components/Navbar/index.tsx
+++ front-web/src/core/components/Navbar/index.tsx
@@ -22,7 +22,7 @@ const Navbar = () => (
 
                 </li>
                 <li>
-                    <NavLink to="/catalog" activeClassName="active">
+                    <NavLink to="/products" activeClassName="active">
                         CATÁLOGO
                     </NavLink>
 
